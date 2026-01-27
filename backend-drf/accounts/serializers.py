5c38351fe0a4bdf1de6779eefe51a279942e3b0f
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-27 14:53:51 +0545

    solved

--- backend-drf/accounts/serializers.py
+++ backend-drf/accounts/serializers.py
@@ -3,12 +3,14 @@ from rest_framework import serializers
 
 
 
-class UserSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
         
     def create(self, validated_data):
+        #User.objects.create = save the password in a plain text
+        #User.objects.create_user = automatically hash the password
         user = User.objects.create_user(**validated_data)
         return user
--- backend-drf/accounts/views.py
+++ backend-drf/accounts/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from .serializers import UserSerializer
-from rest_framework import generics
+from .serializers import RegisterSerializer
+from rest_framework import generics, status
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 
-class RegisterView(generics.CreateAPIview):
+class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serialzer_class= UserSerializer
+    serializer_class= RegisterSerializer
     permission_classes=[AllowAny]
+    
+   
