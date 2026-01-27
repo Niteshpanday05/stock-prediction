c9343959aee4a114642b65a580ad2bc366aa0ba5
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-27 15:10:59 +0545

    register api created

--- backend-drf/accounts/serializers.py
+++ backend-drf/accounts/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, min_length=8, style={'input_type':'password'})
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
