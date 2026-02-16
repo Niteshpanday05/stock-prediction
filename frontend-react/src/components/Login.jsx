633673a251332de36b43db2abd257919e36fca80
Niteshpanday05 <eagleseye.np@gmail.com>
2026-02-16 21:28:38 +0545

    register page done

--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      <p>this is login page
-        
+      <p className='text-light container'>this is login page
+
       </p>
     </div>
   )
 }
 
-export default Login
+export default Login;
--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>
-      Register
+   <>
+   <div className="container">
+    <div className="row justify-content-center">
+      <div className="col-md-6">
+        <h3 className="text-light text-center">Create an Account</h3>
+        <form>
+          <input type="text" className='form-control mb-3' placeholder='Enter username' />
+          <input type="Email" className='form-control mb-3' placeholder='Enter email address' />
+           <input type="Password" className='form-control mb-3' placeholder='Enter password' />
+           <button type='Submit' className='btn btn-info d-block mx-auto'>Register</button>
+        </form>
+      </div>
     </div>
+   </div>
+   </>
   )
 }
 
