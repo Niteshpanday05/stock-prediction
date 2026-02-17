18d29eb2e3eb1d6ac1eab432c00302a52cdcaae3
Niteshpanday05 <eagleseye.np@gmail.com>
2026-02-17 13:06:55 +0545

    implementing axiox

--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -1,17 +1,26 @@
 import React, {useState} from 'react'
+import axios from "axios"
 
 const Register = () => {
   const[username, setUsername] = useState('')
   const[email,setEmail] = useState('')
   const[password, setPassword] = useState('')
 
-  const handleRegistration = (e) => {
+  const handleRegistration =async (e) => {
     e.preventDefault();
 
     const userData = {
       username,email,password
     }
-    console.log('userData==>',userData);
+    
+    try{
+      const response =await axios.post('http://127.0.0.1:8000/api/v1/register/', userData)
+      console.log('response.data==>', response.data)
+      console.log('Registration successful');
+
+    }catch(error){
+        console.error('Registration failed', error.response.data)
+    }
   }
 
 
